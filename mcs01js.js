2f045ba7d71cf36f6ae9d4d58154175c5c82295b
Ashish Kushwaha <ashishkshwh@gmail.com>
2020-10-08 08:03:32 -0700

    pre-final

--- mcs01js.js
+++ mcs01js.js
@@ -1,3 +1,4 @@
+/*
 $("[data-trigger]").on("click", function(){
     var trigger_id =  $(this).attr('data-trigger');
     $(trigger_id).toggleClass("show");
@@ -8,4 +9,4 @@ $("[data-trigger]").on("click", function(){
 $(".btn-close").click(function(e){
     $(".navbar-collapse").removeClass("show");
     $("body").removeClass("offcanvas-active");
-});
+});*/

8bc9b8c8ed0c212cc045c0db794d7b38749a692d
Ashish Kushwaha <ashishkshwh@gmail.com>
2020-11-23 23:49:45 -0800

    adding pictures

--- mcs01js.js
+++ mcs01js.js
@@ -1,3 +1,14 @@
+var cont_div_width = document.getElementById('vision_pic');
+
+if (document.body.clientWidth > 992) {
+	cont_div_width.style.height = (2/3)*cont_div_width.offsetWidth + 'px';
+}
+else {
+	cont_div_width.style.height = (0.47)*cont_div_width.offsetWidth + 'px';
+}
+
+
+
 /*
 $("[data-trigger]").on("click", function(){
     var trigger_id =  $(this).attr('data-trigger');

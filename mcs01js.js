66f100143018d48b62356880ac9ce28700c9fecf
Ashish Kushwaha <ashishkshwh@gmail.com>
2020-11-25 19:17:27 -0800

    adding pictures2

--- mcs01js.js
+++ mcs01js.js
@@ -1,11 +1,39 @@
-var cont_div_width = document.getElementById('vision_pic');
+var mission_div = document.getElementById('mission_pic');
+var vision_div = document.getElementById('vision_pic');
+var goals_div = document.getElementById('goals_pic');
 
-if (document.body.clientWidth > 992) {
-	cont_div_width.style.height = (2/3)*cont_div_width.offsetWidth + 'px';
+/*
+function setpic_height(xElement) {
+	if (document.body.clientWidth > 768) {
+		xElement.style.height = (2/3)*xElement.offsetWidth + 'px';
+	}
+	else {
+		xElement.style.height = (0.45)*xElement.offsetWidth + 'px';
+	}
 }
-else {
-	cont_div_width.style.height = (0.47)*cont_div_width.offsetWidth + 'px';
+
+window.onload = setpic_height(mission_div);
+window.onload = setpic_height(vision_div);
+window.addEventListener('resize', setpic_height(mission_div));
+window.addEventListener('resize', setpic_height(vision_div));
+*/
+
+function setpic_height() {
+	if (document.body.clientWidth > 768) {
+		mission_div.style.height = (2/3)*mission_div.offsetWidth + 'px';
+		vision_div.style.height = (2/3)*vision_div.offsetWidth + 'px';
+		goals_div.style.height = (2/3)*goals_div.offsetWidth + 'px';
+	}
+	else {
+		mission_div.style.height = (0.45)*mission_div.offsetWidth + 'px';
+		vision_div.style.height = (0.45)*vision_div.offsetWidth + 'px';
+		goals_div.style.height = (0.45)*goals_div.offsetWidth + 'px';
+	}
 }
+
+window.onload = setpic_height();
+window.addEventListener('resize', setpic_height);
+
 
 
 
